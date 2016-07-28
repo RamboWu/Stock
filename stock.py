3fc34f6257dc9c70503d8e21bbb64eed3429fde5
Rambo Wu <RamboWu90@gmail.com>
2016-07-28 21:43:46 +0800

    增在打算把曲线画出来

--- stock.py
+++ stock.py
@@ -1,21 +1,13 @@
 #  -*- coding: utf-8 -*-
 #!/usr/bin/python
 
-'''
-from yahoo_finance import Share
-from matplotlib import pylab
-import numpy as np
-import pandas as pd
-import DataAPI
-import seaborn as sns
-sns.set_style('white')
-
-yahoo = Share('YHOO')
-print( yahoo.get_historical('2016-06-25', '2016-07-27') )
-'''
 import pandas_datareader.data as web
 import datetime
 import requests_cache
+import seaborn as sns
+import numpy as np
+from matplotlib import pylab
+import pandas as pd
 
 expire_after = datetime.timedelta(days=3)
 session = requests_cache.CachedSession(cache_name='cache', backend='sqlite', expire_after=expire_after)
@@ -32,5 +24,9 @@ for x in range(len(df)):
 
 df['dates'] = dates
 
-print(df.head())
-print(df.tail())
+print(df['Adj Close'].tail())
+
+df['Adj Close'].plot(grid=False, figsize=(12,8))
+sns.despine()
+#print(df.head())
+#print(df.tail())
