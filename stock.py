dd5b13c817a1fdb472e3f940d2e99a653e226dfa
Rambo Wu <RamboWu90@gmail.com>
2016-07-29 12:47:04 +0800

    去掉了没用的dates

--- stock.py
+++ stock.py
@@ -13,24 +13,13 @@ import pandas as pd
 expire_after = datetime.timedelta(days=3)
 session = requests_cache.CachedSession(cache_name='cache', backend='sqlite', expire_after=expire_after)
 
-start = datetime.datetime(2013, 1, 1)
+start = datetime.datetime(2005, 1, 1)
 end = time.strftime( '%Y-%m-%d', time.localtime() )
 df = web.DataReader("000001.ss", 'yahoo', start, end, session=session)
-
-dates =[]
-for x in range(len(df)):
-    newdate = str(df.index[x])
-    newdate = newdate[0:10]
-    dates.append(newdate)
-
-df['dates'] = dates
-
-print(df['Adj Close'].tail())
 
 window_short = 20
 window_long = 120
 SD = 0.05
-
 
 df['short_window'] = np.round(df['Adj Close'].rolling(window=window_short,center=False).mean(), 4)
 df['long_window'] = np.round(df['Adj Close'].rolling(window=window_long,center=False).mean(), 4)
